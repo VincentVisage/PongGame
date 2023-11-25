f2c91480694ff5d7c82a8726d39b04f94acee957
Jaralva <zaingrazes@mail.ru>
2023-11-25 14:06:15 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
+from scoreboard import ScoarBoard
+import time
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.tracer(0)
+
+
+paddle_r = Paddle(350)
+paddle_l = Paddle(-350)
+ball = Ball()
+scoreboard = ScoarBoard()
+
+screen.listen()
+screen.onkey(paddle_r.go_up, "Up")
+screen.onkey(paddle_r.go_down, "Down")
+screen.onkey(paddle_l.go_up, "w")
+screen.onkey(paddle_l.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    ball.move()
+
+    #Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    #Detect collision with paddle
+    if ball.distance(paddle_r) < 50 and ball.xcor() > 320 or ball.distance(paddle_l) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    #Detect R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    #Detect L paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+
+
+screen.exitonclick()
---
+++ paddle.py
@@ -0,0 +1,27 @@
+from  turtle import Turtle
+
+RIGHT= 350
+LEFT= -350
+
+class Paddle(Turtle):
+    def __init__(self, side):
+        super().__init__()
+        self.color("white")
+        self.shape("square")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(side, 0)
+
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
+
+
+
+
+
